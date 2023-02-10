5259b6595df4905a46c6c30ba4ea4c6aac62751b
KARTHIK KUMAR <kk007@uber.com>
2023-02-10 12:03:35 +0530

    todo app

---
+++ flask/setup/setup.sh
@@ -0,0 +1,20 @@
+ROOT=${WORKSPACE:-$(git rev-parse --show-toplevel)}
+DIR_APP="$ROOT/flask/setup"
+DIR_VENV="$DIR_APP/venv"
+PYTHON="/usr/bin/python3"
+
+
+function setup_venv() {
+    export PIP_CONFIG_FILE=$HOME/.config/pip/pip.conf
+    echo "$PIP_CONFIG_FILE"
+
+    "$PYTHON" -m venv "$DIR_VENV"
+    source "$DIR_VENV/bin/activate"
+  
+    echo "=====checking active virtual environment..."
+    echo "$VIRTUAL_ENV"
+
+    pip install -r "$DIR_APP/requirements.txt"
+}
+
+setup_venv
